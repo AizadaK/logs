654708c41724e7c0a1251185ad30274eb637d82f
Aizada <kasenova.aizada@gmail.com>
2020-10-24 01:24:23 +0600

    created project logs

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from core.models import Article
+def homepage(requiest):
+    return HttpResponse("hi!")
+
+def test(request):
+    return render(request, "test.html")
+
+def articles(request):
+    all_articles = Article.objects.all()
+    return render(request, "articles.html", {"articles" : all_articles})
